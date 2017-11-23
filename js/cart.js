7c361fc149c8456be8e7e03b744ed88cd85ce5d1
LanshanYe <605123483@qq.com>
2017-11-23 19:02:34 +0800

    第二次

--- js/cart.js
+++ js/cart.js
@@ -11,11 +11,16 @@ $.post("http://h6.duchengjiu.top/shop/api_user.php",
 			type:"get",
 			url:url,
 			async:true,
-			success:function(obj){
+			success:function(respones){
+				
+				var h = ""
+				var obj = respones.data[0]
 				console.log(obj)
+				h += '<ul class="shopBox"><li class="checkAll"><input type="checkbox" name="" id="choseALL"  /></li><li class="message"><img src="'+obj.goods_thumb+'" /><span>'+obj.goods_name+'</span></li><li class="price">'+obj.goods_price+'</li><li class="numb"><button>+</button><input type="text" value="'+obj.goods_number+'"/><button>-</button></li><li class="sumb">'+obj.goods_price*obj.goods_number+'</li><li class="delate">删除</li></ul>'
+				$("#shopShow").append(h)
 			}
 		});
 	})
 
-
+	
 
